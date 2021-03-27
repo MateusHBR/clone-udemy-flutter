62338708a5c12e770d796d9c1c1f853802a7720f
Mateus de Morais Ramalho <mateus.morais@cubos.io>
2021-03-27 18:53:20 -0300

    feat: implements course item

--- lib/widgets/curse_item.dart
+++ lib/widgets/curse_item.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class CurseItem extends StatelessWidget {
@@ -10,11 +11,14 @@ class CurseItem extends StatelessWidget {
           'https://images.unsplash.com/photo-1584438784894-089d6a62b8fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
           fit: BoxFit.fitWidth,
         ),
-        Text(
-          'Criação de Apps Android e iOS com Flutter',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 15,
+        Flexible(
+          child: AutoSizeText(
+            'Criação de Apps Android e iOS com Flutter',
+            maxLines: 2,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+            ),
           ),
         ),
         Text(
